1abf26cf3e3fbf043634d3a9e0eb719f854dd4a6
PC <m.mohamedkhalaf240@gmail.com>
2025-07-03 18:11:40 +0300

    search

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var request = new XMLHttpRequest();
 var cityName = document.querySelector("#cityName");
-var tempToday = document.querySelector("#tempToday"); 
+var tempToday = document.querySelector("#tempToday");
 var todayStatusIcon = document.querySelector("#todayStatusIcon");
 var todayStatusText = document.querySelector("#todayStatusText");
 var todayName = document.querySelector("#todayName");
@@ -15,7 +15,9 @@ var tomorrow2StatusIcon = document.querySelector("#tomorrow2StatusIcon");
 var tomorrow2Status = document.querySelector("#tomorrow2Status");
 var tomorrow2MaxTemp = document.querySelector("#tomorrow2MaxTemp");
 var tomorrow2MinTemp = document.querySelector("#tomorrow2MinTemp");
-request.open("GET","http://api.weatherapi.com/v1/forecast.json?key=2f8decbf816447469f8142742242404&q=London&days=3&aqi=no&alerts=no");
+var searchBar = document.querySelector("#searchBar");
+
+request.open("GET", "http://api.weatherapi.com/v1/forecast.json?key=2f8decbf816447469f8142742242404&q=London&days=3&aqi=no&alerts=no");
 
 request.send();
 request.responseType = "json"
@@ -39,11 +41,11 @@ const options = { weekday: 'long' };
 const nameTomorrow = tomorrow.toLocaleString('default', options);
 const dayAfterTomorrowName = dayAfterTomorrow.toLocaleString('default', options);
 
-request.addEventListener("load",function(){
+request.addEventListener("load", function () {
     console.log(request.response);
     cityName.textContent = request.response.location.name;
     tempToday.innerHTML = `${request.response.current.temp_c}<sup>o</sup> C`;
-    todayStatusIcon.setAttribute("src",`https:${request.response.current.condition.icon}`);
+    todayStatusIcon.setAttribute("src", `https:${request.response.current.condition.icon}`);
     todayStatusText.textContent = request.response.current.condition.text;
     todayName.textContent = dayName;
     todayDate.textContent = dayNumber + " " + monthName;
@@ -51,10 +53,21 @@ request.addEventListener("load",function(){
     tomorrow2Name.textContent = dayAfterTomorrowName;
     tomorrowMaxTemp.textContent = request.response.forecast.forecastday[1].day.maxtemp_c;
     tomorrowMinTemp.textContent = request.response.forecast.forecastday[1].day.mintemp_c;
-    tomorrowStatusIcon.setAttribute("src",`https:${request.response.forecast.forecastday[1].day.condition.icon}`);
+    tomorrowStatusIcon.setAttribute("src", `https:${request.response.forecast.forecastday[1].day.condition.icon}`);
     tomorrowStatus.textContent = request.response.forecast.forecastday[1].day.condition.text;
     tomorrow2MaxTemp.textContent = request.response.forecast.forecastday[2].day.maxtemp_c;
     tomorrow2MinTemp.textContent = request.response.forecast.forecastday[2].day.mintemp_c;
-    tomorrow2StatusIcon.setAttribute("src",`https:${request.response.forecast.forecastday[2].day.condition.icon}`);
+    tomorrow2StatusIcon.setAttribute("src", `https:${request.response.forecast.forecastday[2].day.condition.icon}`);
     tomorrow2Status.textContent = request.response.forecast.forecastday[2].day.condition.text;
+})
+searchBar.addEventListener("input", function () {
+    if(searchBar.value.trim()){
+        request.open("GET", `http://api.weatherapi.com/v1/forecast.json?key=2f8decbf816447469f8142742242404&q=${searchBar.value}&days=3&aqi=no&alerts=no`);
+    }
+    else{
+        request.open("GET", "http://api.weatherapi.com/v1/forecast.json?key=2f8decbf816447469f8142742242404&q=London&days=3&aqi=no&alerts=no");
+    }
+    request.send();
+    request.responseType = "json";
+
 })
